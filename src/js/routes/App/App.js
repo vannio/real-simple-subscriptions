4479df6eed0d37fb412c2e9897c3b54acb84b6bf
Van <vannio@users.noreply.github.com>
2017-07-31 12:56:23 +0100

    Add react router + simple routes

--- src/js/routes/App/App.js
+++ src/js/routes/App/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+  <Router>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
+        </ul>
+      </div>
+      <div className="App-content">
+        <Route exact path="/" render={() => (
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+        )} />
+        <Route path="/settings" render={() => (
+          <p>Settings here</p>
+        )} />
+      </div>
     </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  </Router>
 );
 
 export default App;
