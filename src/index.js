93176f1cfa879878a7715996384bf9eefde09036
Van <vannio@users.noreply.github.com>
2017-07-31 22:41:37 +0100

    Persist state in localStorage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,28 @@ import reducers from './js/reducers';
 import './index.css';
 import App from './js/routes/App/App';
 import registerServiceWorker from './js/helpers/registerServiceWorker';
+import { loadState, saveState } from './js/helpers/localStorage';
+import throttle from 'lodash/throttle';
 
 const configureStore = () => {
-  const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  const store = createStore(
+    reducers,
+    loadState(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   store.dispatch = store;
+  store.subscribe(
+    throttle(
+      () => {
+        saveState({
+          settings: store.getState().settings,
+          archived: store.getState().archived,
+          subscriptions: store.getState().subscriptions
+        });
+      },
+      1000
+    )
+  );
   return store;
 };
 
