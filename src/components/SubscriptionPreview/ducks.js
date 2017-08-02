63981785f16672e50f37a39da23487b4575b090d
Van <vannio@users.noreply.github.com>
2017-08-03 00:37:28 +0100

    Add 'mark feeditem as read' actions
    Ducks
    
    Mark as read

---
+++ src/components/SubscriptionPreview/ducks.js
@@ -0,0 +1,14 @@
+export const getSubscription = (state, subscriptionId) =>
+  state.subscriptions[subscriptionId];
+
+export const getFeedItemObject = (state, subscriptionId) =>
+  state.feedItems[subscriptionId] || {};
+
+export const getFeedItems = (state, subscriptionId) =>
+  getFeedItemObject(state, subscriptionId).items || [];
+
+export const isFeedItemsFetching = (state, subscriptionId) =>
+  getFeedItemObject(state, subscriptionId).fetching;
+
+export const getFeedItemsFetchError = (state, subscriptionId) =>
+  getFeedItemObject(state, subscriptionId).error;
--- src/reducers/feedItems.js
+++ src/reducers/feedItems.js
@@ -7,21 +7,31 @@ const feedItems = (state = {}, action) => {
     case FETCH_FEEDITEMS_REQUEST:
       return {
         ...state,
-        fetching: true,
-        error: null
+        [action.subscriptionId]: {
+          ...state[action.subscriptionId],
+          fetching: true,
+          error: null
+        }
       };
     case FETCH_FEEDITEMS_SUCCESS:
       return {
         ...state,
-        fetching: false,
-        error: null,
-        [action.subscriptionId]: action.items
+        [action.subscriptionId]: {
+          ...state[action.subscriptionId],
+          items: action.items,
+          fetching: false,
+          error: null
+        }
       };
     case FETCH_FEEDITEMS_FAILURE:
       return {
         ...state,
-        fetching: false,
-        error: action.error
+        [action.subscriptionId]: {
+          ...state[action.subscriptionId],
+          items: action.items,
+          fetching: false,
+          error: action.error
+        }
       };
     default:
       return state;
